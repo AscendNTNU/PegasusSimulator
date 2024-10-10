3fc53a1fd80caa9d64c56e2054480a9ae295a4e0
EdvardSire <mail@edvardsire.com>
2024-10-10 21:56:57 +0200

    lidartest

---
+++ lidartest.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+
+
+WORLD_FILE = "/home/ascend/Documents/autonomous_flight_world.usd"
+
+
+
+"""
+| File: 8_camera_vehicle.py
+| License: BSD-3-Clause. Copyright (c) 2024, Marcelo Jacinto. All rights reserved.
+| Description: This files serves as an example on how to build an app that makes use of the Pegasus API, 
+| where the data is send/received through mavlink, the vehicle is controled using mavlink and
+| camera data is sent to ROS2 topics at the same time.
+"""
+
+# Imports to start Isaac Sim from this script
+import carb
+from isaacsim import SimulationApp
+
+# Start Isaac Sim's simulation environment
+# Note: this simulation app must be instantiated right after the SimulationApp import, otherwise the simulator will crash
+# as this is the object that will load all the extensions and load the actual simulator.
+simulation_app = SimulationApp({"headless": False})
+
+# -----------------------------------
+# The actual script should start here
+# -----------------------------------
+import omni.timeline
+from omni.isaac.core.world import World
+
+# Import the Pegasus API for simulating drones
+from pegasus.simulator.params import ROBOTS, SIMULATION_ENVIRONMENTS
+from pegasus.simulator.logic.graphical_sensors.monocular_camera import MonocularCamera
+from pegasus.simulator.logic.graphical_sensors.lidar import Lidar
+from pegasus.simulator.logic.backends.mavlink_backend import MavlinkBackend, MavlinkBackendConfig
+from pegasus.simulator.logic.backends.ros2_backend import ROS2Backend
+from pegasus.simulator.logic.vehicles.multirotor import Multirotor, MultirotorConfig
+from pegasus.simulator.logic.interface.pegasus_interface import PegasusInterface
+
+# Auxiliary scipy and numpy modules
+from scipy.spatial.transform import Rotation
+
+class PegasusApp:
+    """
+    A Template class that serves as an example on how to build a simple Isaac Sim standalone App.
+    """
+
+    def __init__(self):
+        """
+        Method that initializes the PegasusApp and is used to setup the simulation environment.
+        """
+
+        # Acquire the timeline that will be used to start/stop the simulation
+        self.timeline = omni.timeline.get_timeline_interface()
+
+        # Start the Pegasus Interface
+        self.pg = PegasusInterface()
+
+        # Acquire the World, .i.e, the singleton that controls that is a one stop shop for setting up physics,
+        # spawning asset primitives, etc.
+        self.pg._world = World(**self.pg._world_settings)
+        self.world = self.pg.world
+
+        # Launch one of the worlds provided by NVIDIA
+        #self.pg.load_environment(SIMULATION_ENVIRONMENTS["Curved Gridroom"])
+        self.pg.load_environment(WORLD_FILE)
+
+        from omni.isaac.core.objects import DynamicCuboid
+
+        # Create the vehicle
+        # Try to spawn the selected robot in the world to the specified namespace
+        config_multirotor = MultirotorConfig()
+        # Create the multirotor configuration
+        mavlink_config = MavlinkBackendConfig({
+            "vehicle_id": 0,
+            "px4_autolaunch": True,
+            "px4_dir": "/home/ascend/PX4-Autopilot"
+        })
+        config_multirotor.backends = [
+            MavlinkBackend(mavlink_config), 
+            ROS2Backend(vehicle_id=1, 
+                        config={
+                            "namespace": 'drone', 
+                            "pub_sensors": True,
+                            "pub_imu": True,
+                            "pub_graphical_sensors": True,
+                            "pub_state": True,
+                            "sub_control": False,
+                            "pub_tf": True})]
+
+        # Create a camera and lidar sensors
+        import numpy as np
+        camera_config = {
+            "depth": True,
+            #"position": np.array([0.30, 0.0, 0.0]),
+            #"orientation": np.array([0.0, 0.0, 0.0]),
+            #"resolution": (1920, 1200),
+            #"frequency": 30,
+            #"intrinsics": np.array([[5, 0.0, 957.8], [0.0, 5, 589.5], [0.0, 0.0, 1.0]]), # DOESN'T DO ANYTHING
+            #"intrinsics": np.array([[958.8, 0.0, 957.8], [0.0, 956.7, 589.5], [0.0, 0.0, 1.0]]),
+            #"distortion_coefficients": np.array([0.14, -0.03, -0.0002, -0.00003, 0.009, 0.5, -0.07, 0.017]),
+            #"diagonal_fov": 140.0
+        }
+
+        self.camera = MonocularCamera("camera", config=camera_config)
+        self.lidar = Lidar("lidar")
+        config_multirotor.graphical_sensors = [self.camera, self.lidar] # Lidar("lidar")
+
+        
+        Multirotor(
+            "/World/quadrotor",
+            ROBOTS['Iris'],
+            0,
+            [0.0, 0.0, 0.07],
+            Rotation.from_euler("XYZ", [0.0, 0.0, 0.0], degrees=True).as_quat(),
+            config=config_multirotor,
+        )
+
+        # Reset the simulation environment so that all articulations (aka robots) are initialized
+        self.world.reset()
+
+        # Auxiliar variable for the timeline callback example
+        self.stop_sim = False
+
+    def run(self):
+        """
+        Method that implements the application main loop, where the physics steps are executed.
+        """
+
+        self.camera._camera.set_focal_length(12.5 / 10)
+
+        # Start the simulation
+        self.timeline.play()
+
+        # The "infinite" loop
+        while simulation_app.is_running() and not self.stop_sim:
+            # Update the UI of the app and perform the physics step
+            self.world.step(render=True)
+
+        # Cleanup and stop
+        carb.log_warn("PegasusApp Simulation App is closing.")
+        self.timeline.stop()
+        simulation_app.close()
+
+def main():
+
+    # Instantiate the template app
+    pg_app = PegasusApp()
+
+    # Run the application loop
+    pg_app.run()
+
+if __name__ == "__main__":
+    main()
